04ffe206b28684cff9c5381dc41bbc32d4e0816a
Newman Ferrer <newmanferrer@gmail.com>
2022-08-30 13:22:33 -0400

    test: :fire: delete App.test.tsx file
    delete App.test.tsx file

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { App } from './App';
-
-describe('test App Component', () => {
-  it('test #1: should the text "App", in the document', () => {
-    render(<App />);
-    expect(screen.getByRole('heading', { level: 1, name: /^App$/ })).toBeInTheDocument();
-  });
-});
