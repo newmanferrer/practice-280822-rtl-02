0f8382a63d10f0941522bfb3108a0d087a23ac58
Newman Ferrer <newmanferrer@gmail.com>
2022-08-30 13:25:28 -0400

    test: :sparkles: create test file
    create test file

---
+++ src/components/SeriesList/SeriesList.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen, act, RenderResult } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { SeriesList } from '../';
+
+describe('Test SeriesList Component', () => {
+  it('test #1: should load images of the series', async () => {
+    expect(true).toBe(true);
+  });
+});
+
+// screen.getByRole('img', {  name: /juego de tronos/i})
