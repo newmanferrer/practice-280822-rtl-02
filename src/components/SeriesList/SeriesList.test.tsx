07572c65cae5826cfa8b0d331fbfc60bfc86ed18
Newman Ferrer <newmanferrer@gmail.com>
2022-08-30 21:40:59 -0400

    test: add new test
    add new test

--- src/components/SeriesList/SeriesList.test.tsx
+++ src/components/SeriesList/SeriesList.test.tsx
@@ -1,11 +1,24 @@
-import { render, screen, act, RenderResult } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { render, RenderResult } from '@testing-library/react';
 import { SeriesList } from '../';
 
 describe('Test SeriesList Component', () => {
-  it('test #1: should load images of the series', async () => {
-    expect(true).toBe(true);
+  let renderMethods: RenderResult;
+
+  beforeEach(async () => {
+    renderMethods = render(<SeriesList />);
+  });
+
+  it('test #1: should load three (3) images of the series', async () => {
+    const { findAllByRole } = renderMethods;
+    expect(await findAllByRole('img')).toHaveLength(3);
+  });
+
+  it('test #2: should show serie "game of thrones", in the documen', async () => {
+    const { findByRole, findByText } = renderMethods;
+
+    expect(
+      await findByRole('heading', { level: 2, name: /^Juego de Tronos$/ })
+    ).toBeInTheDocument();
+    expect(await findByText(/^Juego de Tronos$/)).toBeInTheDocument();
   });
 });
-
-// screen.getByRole('img', {  name: /juego de tronos/i})
