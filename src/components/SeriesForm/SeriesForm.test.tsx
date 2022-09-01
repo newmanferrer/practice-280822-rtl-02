109c183beb0da68a3ff0dd490251f4b5e54229aa
Newman Ferrer <newmanferrer@gmail.com>
2022-09-01 19:52:12 -0400

    test: :sparkles: add news tests
    add news tests

--- src/components/SeriesForm/SeriesForm.test.tsx
+++ src/components/SeriesForm/SeriesForm.test.tsx
@@ -1,24 +1,34 @@
-import { render, screen, RenderResult, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import {
+  render,
+  screen,
+  RenderResult,
+  waitFor,
+  waitForElementToBeRemoved
+} from '@testing-library/react';
+import user from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 import { SeriesForm } from '..';
 
 describe('Test SeriesForm Component', () => {
   let renderMethods: RenderResult;
 
-  beforeEach(async () => {
+  beforeEach(() => {
     renderMethods = render(<SeriesForm />);
   });
 
-  it('test #1: should six inputs in the form', () => {
+  it('test #1: should show six inputs in the form', () => {
     const { getAllByRole } = renderMethods;
 
     expect(getAllByRole('spinbutton')).toHaveLength(1);
     expect(getAllByRole('textbox')).toHaveLength(5);
   });
 
-  it('test #2: should create a new serie', async () => {
-    const { getByRole, getByLabelText } = renderMethods;
-    const user = userEvent.setup();
+  it('test #2: should show the button "Create Series", in the form', () => {
+    const { getByRole } = renderMethods;
+    expect(getByRole('button', { name: /^Create Serie$/ })).toBeInTheDocument();
+  });
+
+  it('test #3: should create a new series, the success message appears and then disappears.', async () => {
     const dataForm = {
       title: 'My serie title',
       creator: 'My serie creator',
@@ -33,24 +43,47 @@ describe('Test SeriesForm Component', () => {
     //* with the input. We can also use getByLabelText to get the input.
     //* Let's remember the priority: 1.- getByRole, 2.- getByLabelText
     //* ---------------------------------------------------------------------
-    const inputTitle = getByRole('textbox', { name: /^Title$/ });
-    const inputCreator = getByLabelText(/^Creator$/, { selector: 'input' });
+    const inputTitle = screen.getByRole('textbox', { name: /^Title$/ });
+    const inputCreator = screen.getByLabelText(/^Creator$/, { selector: 'input' });
 
-    const inputRatin = getByRole('spinbutton', { name: /^Ratin$/ });
-    const inputDates = getByRole('textbox', { name: /^Dates$/ });
-    const inputImage = getByRole('textbox', { name: /^Image$/ });
-    const inputChannel = getByRole('textbox', { name: /^Channel$/ });
-    const buttonCreate = getByRole('button', { name: /^Create Serie$/ });
+    const inputRatin = screen.getByRole('spinbutton', { name: /^Ratin$/ });
+    const inputDates = screen.getByRole('textbox', { name: /^Dates$/ });
+    const inputImage = screen.getByRole('textbox', { name: /^Image$/ });
+    const inputChannel = screen.getByRole('textbox', { name: /^Channel$/ });
+    const buttonCreate = screen.getByRole('button', { name: /^Create Serie$/ });
     //* ---------------------------------------------------------------------
 
-    user.type(inputTitle, dataForm.title);
-    user.type(inputCreator, dataForm.creator);
-    user.type(inputRatin, dataForm.ratin);
-    user.type(inputDates, dataForm.dates);
-    user.type(inputImage, dataForm.image);
-    user.type(inputChannel, dataForm.channel);
-    user.click(buttonCreate);
+    //* ---------------------------------------------------------------------
+    //* Notes: Since fields "title" and "creator" are required, they must be
+    //* handled in a special way. With "await" we wait until the required fields are
+    //* filled before pressing the button and submitting the form. With "act"
+    //* we eliminate the warning for changes of state "setSerieCreated" and
+    //* "setForm".
+    //* ---------------------------------------------------------------------
+    await act(async () => {
+      await user.type(inputTitle, dataForm.title);
+      await user.type(inputCreator, dataForm.creator);
+      await user.type(inputRatin, dataForm.ratin);
+      await user.type(inputDates, dataForm.dates);
+      await user.type(inputImage, dataForm.image);
+      await user.type(inputChannel, dataForm.channel);
+      await user.click(buttonCreate);
+    });
+    //* ---------------------------------------------------------------------
 
-    // const messageSuccess = await screen.findByText(/series created successfully/i);
+    await waitFor(() => {
+      screen.getByRole('divMessageSuccess', { name: /^messageSuccess$/ });
+    });
+
+    expect(screen.getByRole('divMessageSuccess', { name: /^messageSuccess$/ })).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(
+      () => screen.queryByRole('divMessageSuccess', { name: /^messageSuccess$/ }),
+      { timeout: 2000 }
+    );
+
+    expect(
+      screen.queryByRole('divMessageSuccess', { name: /^messageSuccess$/ })
+    ).not.toBeInTheDocument();
   });
 });
