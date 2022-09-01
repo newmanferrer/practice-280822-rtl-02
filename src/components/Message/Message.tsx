2e8c65141301823fc6b65cf771d7c4edcd784e00
Newman Ferrer <newmanferrer@gmail.com>
2022-09-01 19:40:16 -0400

    feat: :sparkles: add role and arial-laber props
    add role and arial-laber props

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -3,12 +3,14 @@ import { MessageWrapper, MessageStyled } from './StyledComponents';
 interface MessageProps {
   type?: 'success' | 'warn' | 'error';
   text: string;
+  role?: string;
+  ariaLabel?: string;
   dataTestid?: string;
 }
 
-export const Message = ({ type, text, dataTestid }: MessageProps) => {
+export const Message = ({ type, text, role, ariaLabel, dataTestid }: MessageProps) => {
   return (
-    <MessageWrapper data-testid={dataTestid} type={type}>
+    <MessageWrapper type={type} role={role} aria-label={ariaLabel} data-testid={dataTestid}>
       <MessageStyled type={type}>{text}</MessageStyled>
     </MessageWrapper>
   );
