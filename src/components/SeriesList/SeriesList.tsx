f73f3254f78ed7114e767bddc6bfb5f71e889de6
Newman Ferrer <newmanferrer@gmail.com>
2022-09-06 11:14:00 -0400

    refactor: :zap: functions and style of render
    functions and style of render

--- src/components/SeriesList/SeriesList.tsx
+++ src/components/SeriesList/SeriesList.tsx
@@ -1,21 +1,37 @@
 import { useState, useEffect } from 'react';
 import { getAllSeriesFetch } from '../../services';
-import { SerieCard } from '../';
+import { Loader, Message, SerieCard } from '../';
 import { ISerie } from '../../models';
 import { ListWrapper } from './StyledComponents';
 
+const errorInitialState = { hasError: false, message: '' };
+
 export const SeriesList = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [series, setSeries] = useState<ISerie[]>();
+  const [error, setError] = useState(errorInitialState);
 
   useEffect(() => {
+    setError(errorInitialState);
+    setIsLoading(true);
     getAllSeriesFetch()
       .then(data => setSeries(data))
-      .catch(error => console.log(error));
+      .catch(error => handleError(error))
+      .finally(() => setIsLoading(false));
   }, []);
+
+  function handleError(error: any) {
+    setError({ hasError: true, message: error.message });
+  }
 
   return (
     <ListWrapper>
-      {series && series.map(serie => <SerieCard key={serie.id} serie={serie} />)}
+      {isLoading && <Loader />}
+      {!isLoading && error.hasError && <Message type='error' text={error.message} />}
+      {!isLoading &&
+        !error.hasError &&
+        series &&
+        series.map(serie => <SerieCard key={serie.id} serie={serie} />)}
     </ListWrapper>
   );
 };
